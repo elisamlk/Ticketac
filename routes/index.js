98bbf225d6c49bed5cc02d859adddb94d0d0229f
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2022-04-14 17:38:41 +0200

    Ajout de la page lastTrip.ejs et tickets.ejs

--- routes/index.js
+++ routes/index.js
@@ -39,108 +39,103 @@ router.get("/login", function (req, res, next) {
   res.render("login");
 });
 
-// POST signup page // 
-router.post('/signup', async function(req,res,next){
-
+// POST signup page //
+router.post("/signup", async function (req, res, next) {
   var searchUser = await signupModel.findOne({
-    email: req.body.emailFromFront
-  })
-  
-  if(!searchUser){
+    email: req.body.emailFromFront,
+  });
+
+  if (!searchUser) {
     var newUser = new signupModel({
       name: req.body.nameFromFront,
       firstname: req.body.firstnameFromFront,
       email: req.body.emailFromFront,
       password: req.body.passwordFromFront,
-    })
-  
+    });
+
     var newUserSave = await newUser.save();
-  
+
     req.session.user = {
       name: newUserSave.username,
       id: newUserSave._id,
-    }
-  
-    console.log(req.session.user)
-  
-    res.redirect('/homepage')
+    };
+
+    console.log(req.session.user);
+
+    res.redirect("/homepage");
   } else {
-    res.redirect('/')
+    res.redirect("/");
   }
-  
 });
 
 // POST signin page //
 
-router.post('/signin', async function(req,res,next){
-
+router.post("/signin", async function (req, res, next) {
   var searchUser = await signinModel.findOne({
     email: req.body.emailFromFront,
-    password: req.body.passwordFromFront
-  })
+    password: req.body.passwordFromFront,
+  });
 
-  if(searchUser!= null){
+  if (searchUser != null) {
     req.session.user = {
       name: searchUser.username,
-      id: searchUser._id
-    }
-    res.redirect('/homepage')
+      id: searchUser._id,
+    };
+    res.redirect("/homepage");
   } else {
-    res.render('login')
+    res.render("login");
   }
-
 });
 
-router.get('/logout', function(req,res,next){
-
+router.get("/logout", function (req, res, next) {
   req.session.user = null;
 
-  res.redirect('/')
+  res.redirect("/");
+});
+/*
+// Remplissage de la base de donnée, une fois suffit
+router.get("/save", async function (req, res, next) {
+  // How many journeys we want
+  var count = 300;
+
+  // Save  ---------------------------------------------------
+  for (var i = 0; i < count; i++) {
+    departureCity = city[Math.floor(Math.random() * Math.floor(city.length))];
+    arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))];
+
+    if (departureCity != arrivalCity) {
+      var newUser = new journeyModel({
+        departure: departureCity,
+        arrival: arrivalCity,
+        date: date[Math.floor(Math.random() * Math.floor(date.length))],
+        departureTime: Math.floor(Math.random() * Math.floor(23)) + ":00",
+        price: Math.floor(Math.random() * Math.floor(125)) + 25,
+      });
+
+      await newUser.save();
+    }
+  }
+  res.render("index", { title: "Express" });
 });
 
-// // Remplissage de la base de donnée, une fois suffit
-// router.get("/save", async function (req, res, next) {
-//   // How many journeys we want
-//   var count = 300;
+// Cette route est juste une verification du Save.
+// Vous pouvez choisir de la garder ou la supprimer.
+router.get("/result", function (req, res, next) {
+  // Permet de savoir combien de trajets il y a par ville en base
+  for (i = 0; i < city.length; i++) {
+    journeyModel.find(
+      { departure: city[i] }, //filtre
 
-//   // Save  ---------------------------------------------------
-//   for (var i = 0; i < count; i++) {
-//     departureCity = city[Math.floor(Math.random() * Math.floor(city.length))];
-//     arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))];
+      function (err, journey) {
+        console.log(
+          `Nombre de trajets au départ de ${journey[0].departure} : `,
+          journey.length
+        );
+      }
+    );
+  }
 
-//     if (departureCity != arrivalCity) {
-//       var newUser = new journeyModel({
-//         departure: departureCity,
-//         arrival: arrivalCity,
-//         date: date[Math.floor(Math.random() * Math.floor(date.length))],
-//         departureTime: Math.floor(Math.random() * Math.floor(23)) + ":00",
-//         price: Math.floor(Math.random() * Math.floor(125)) + 25,
-//       });
-
-//       await newUser.save();
-//     }
-//   }
-//   res.render("index", { title: "Express" });
-// });
-
-// // Cette route est juste une verification du Save.
-// // Vous pouvez choisir de la garder ou la supprimer.
-// router.get("/result", function (req, res, next) {
-//   // Permet de savoir combien de trajets il y a par ville en base
-//   for (i = 0; i < city.length; i++) {
-//     journeyModel.find(
-//       { departure: city[i] }, //filtre
-
-//       function (err, journey) {
-//         console.log(
-//           `Nombre de trajets au départ de ${journey[0].departure} : `,
-//           journey.length
-//         );
-//       }
-//     );
-//   }
-
-//   res.render("index", { title: "Express" });
-// });
-
+  res.render("index", { title: "Express" });
+});
+*/
 module.exports = router;
