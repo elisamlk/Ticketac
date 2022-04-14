ce7555a93335126d0bdd63fb8f1a988e74eb1962
Laurene Pinaud <laurene@nectaar.io>
2022-04-14 11:43:04 +0200

    model users

---
+++ models/users.js
@@ -0,0 +1,21 @@
+// Modèle js users pour le signin et le signup
+
+var mongoose = require('mongoose');
+
+var signinSchema = mongoose.Schema({
+    email: String,
+    password: String,
+})
+
+var signinModel = mongoose.model('users', signinSchema)
+
+var signupSchema = mongoose.Schema({
+    name: String,
+    firstname: String,
+    email: String,
+    password: String,
+})
+
+var signupModel = mongoose.model('users', signupSchema)
+
+module.exports = usersModel;
